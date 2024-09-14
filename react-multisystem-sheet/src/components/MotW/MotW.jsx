d28851fa23b983be3b24dbbd88f52ed8810c973d
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-09-13 22:45:16 -0400

    updated base stats for MotW

--- react-multisystem-sheet/src/components/MotW/MotW.jsx
+++ react-multisystem-sheet/src/components/MotW/MotW.jsx
@@ -3,6 +3,34 @@ import styles from './MotW.module.css'
 
 export const MotW = () => {
     return (
-        <section></section>
+        <section>
+            <h2>Stats</h2>
+            <table>
+                <tr>
+                    <th>Ability</th>
+                    <th>Modifier</th>
+                </tr>
+                <tr>
+                 <td>Charm</td>
+                 <td><input type="number" id="charmMod" max="3"/></td>   
+                </tr>
+                <tr>
+                    <td>Cool</td>
+                    <td><input type="number" name="coolMod" id="coolMod" max="3"/></td>
+                </tr>
+                <tr>
+                    <td>Sharp</td>
+                    <td><input type="number" name="sharpMod" id="sharpMod" max="3"/></td>
+                </tr>
+                <tr>
+                    <td>Tough</td>
+                    <td><input type="number" name="toughMod" id="toughMod" max="3" /></td>
+                </tr>
+                <tr>
+                    <td>Weird</td>
+                    <td><input type="number" name="weirdMod" id="weirdMod" max="3" /></td>
+                </tr>
+            </table>
+        </section>
     )
 }
