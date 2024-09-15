932c00b80fc66c1b079ff768ce649b3d018658b1
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-09-14 23:45:05 -0400

    inserting planning for modals with MotW stats

--- react-multisystem-sheet/src/components/MotW/MotW.jsx
+++ react-multisystem-sheet/src/components/MotW/MotW.jsx
@@ -12,22 +12,27 @@ export const MotW = () => {
                 </tr>
                 <tr>
                  <td>Charm</td>
+                 {/* insert modal for Manipulate Someone */}
                  <td><input type="number" id="charmMod" max="3"/></td>   
                 </tr>
                 <tr>
                     <td>Cool</td>
+                    {/* insert modal for Act Under Pressure and Help Out */}
                     <td><input type="number" name="coolMod" id="coolMod" max="3"/></td>
                 </tr>
                 <tr>
                     <td>Sharp</td>
+                    {/* insert modal for Investigate a Mystery and Read a Bad Situation*/}
                     <td><input type="number" name="sharpMod" id="sharpMod" max="3"/></td>
                 </tr>
                 <tr>
                     <td>Tough</td>
+                    {/* insert modal for Kick Some Ass */}
                     <td><input type="number" name="toughMod" id="toughMod" max="3" /></td>
                 </tr>
                 <tr>
                     <td>Weird</td>
+                    {/* insert modal for Weird moves */}
                     <td><input type="number" name="weirdMod" id="weirdMod" max="3" /></td>
                 </tr>
             </table>
