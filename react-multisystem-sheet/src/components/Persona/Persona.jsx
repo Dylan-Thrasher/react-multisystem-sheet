17e1d8b3118be20d7f7f58c3cf0aa912b0339000
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-09-17 21:55:01 -0400

    inserted planning for base Persona sheet

--- react-multisystem-sheet/src/components/Persona/Persona.jsx
+++ react-multisystem-sheet/src/components/Persona/Persona.jsx
@@ -3,6 +3,14 @@ import styles from './Persona.module.css'
 
 export const Persona = () => {
     return (
-        <section></section>
+        <section>
+            {/* Left Side Player, Right Side Persona */}
+            {/* Player name, Character name, Arcana */}
+            {/* Player stats will be Charm, Academics, Athletics, Willpower, Notice */}
+            {/* Persona stats will be Strength, Magic, Defense, Vitality, Agility */}
+            {/* Insert for Accessing Tarot images and their descriptions. */}
+            {/* List of social links and ranks up to 10 */}
+            {/* Text box for saving various benefits. */}
+        </section>
     )
 }
