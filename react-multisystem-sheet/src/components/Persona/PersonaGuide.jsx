90458450515dcdebe7c0080226ad3bd090acbdae
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-10-06 20:16:53 -0400

    Adding a guide to the Persona rules

---
+++ react-multisystem-sheet/src/components/Persona/PersonaGuide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Persona.module.css'
+
+export const Persona = () => {
+    return (
+        <section>
+            {/* Descriptions of stats*/}
+            {/* Charts for Dice used */}
+            {/* Status Effect Descriptions */}
+        </section>
+    )
+}
