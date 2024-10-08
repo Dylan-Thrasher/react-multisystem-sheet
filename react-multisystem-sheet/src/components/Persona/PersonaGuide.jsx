7615e694e2936fcc84ec55f980040d7dcd34db88
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-10-07 21:11:12 -0400

    adding planning for additional explanations of system being developed in Persona Guide

--- react-multisystem-sheet/src/components/Persona/PersonaGuide.jsx
+++ react-multisystem-sheet/src/components/Persona/PersonaGuide.jsx
@@ -6,7 +6,9 @@ export const Persona = () => {
         <section>
             {/* Descriptions of stats*/}
             {/* Charts for Dice used */}
+            {/* Explanation of Critical Hit */}
             {/* Status Effect Descriptions */}
+            {/* Explanation of status effect removing spells vs healing use */}
         </section>
     )
 }
