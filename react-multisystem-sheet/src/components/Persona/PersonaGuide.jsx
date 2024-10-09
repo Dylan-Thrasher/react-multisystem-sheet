5c826f8afbddc2fd8b06365e198b3da80ce90404
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-10-09 16:36:59 -0400

    adding Persona Forms concept to guide

--- react-multisystem-sheet/src/components/Persona/PersonaGuide.jsx
+++ react-multisystem-sheet/src/components/Persona/PersonaGuide.jsx
@@ -5,6 +5,7 @@ export const Persona = () => {
     return (
         <section>
             {/* Descriptions of stats*/}
+            {/* Possibility of Persona Forms being a perk such as flying, vehicle, humanoid */}
             {/* Resist vs Weakness for elements*/}
             {/* Charts for Dice used */}
             {/* Explanation of Critical Hit */}
