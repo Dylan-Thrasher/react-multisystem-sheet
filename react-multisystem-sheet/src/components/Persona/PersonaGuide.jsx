dcc98951ccad6b5094651a1ef5b353d3cc7f114d
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-10-08 23:01:28 -0400

    extended guide planning before storm

--- react-multisystem-sheet/src/components/Persona/PersonaGuide.jsx
+++ react-multisystem-sheet/src/components/Persona/PersonaGuide.jsx
@@ -5,10 +5,14 @@ export const Persona = () => {
     return (
         <section>
             {/* Descriptions of stats*/}
+            {/* Resist vs Weakness for elements*/}
             {/* Charts for Dice used */}
             {/* Explanation of Critical Hit */}
             {/* Status Effect Descriptions */}
             {/* Explanation of status effect removing spells vs healing use */}
+            {/* Social link descriptions */}
+            {/* Level up planning */}
+            {/* Combat round example */}
         </section>
     )
 }
