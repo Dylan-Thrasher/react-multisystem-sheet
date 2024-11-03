1e3e56b9aad918f63f2c433bb100778d3386abb8
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-11-02 22:06:24 -0400

    adding segments for move explanations in MotW sheet

--- react-multisystem-sheet/src/components/MotW/MotW.jsx
+++ react-multisystem-sheet/src/components/MotW/MotW.jsx
@@ -18,27 +18,43 @@ export const MotW = () => {
                     <th>Modifier</th>
                 </tr>
                 <tr>
-                 <td>Charm</td>
+                 <td>Charm<br />
+                <ul>
+                    <li>-Manipulate Someone</li>
+                </ul>
+                 </td>
                  {/* insert modal for Manipulate Someone */}
                  {/* Manipulate Someone requires you to give what you want them to do and why they should */}
                  <td><input type="number" id="charmMod" max="3"/></td>   
                 </tr>
                 <tr>
-                    <td>Cool</td>
+                    <td>Cool<br />
+                <ul>
+                    <li>Act Under Pressure</li>
+                    <li>Help Out</li>
+                </ul></td>
                     {/* insert modal for Act Under Pressure and Help Out */}
                     {/* Act Under Pressure is a reactionary roll, typically requested by Keeper */}
                     {/* Help Out is used to increase another player's roll by +1 */}
                     <td><input type="number" name="coolMod" id="coolMod" max="3"/></td>
                 </tr>
                 <tr>
-                    <td>Sharp</td>
+                    <td>Sharp<br />
+                <ul>
+                    <li>Investigate A Mystery</li>
+                    <li>Read A Bad Situation</li>
+                </ul></td>
                     {/* insert modal for Investigate a Mystery and Read a Bad Situation*/}
                     {/* Investigate a Mystery is used to investigate when not in danger */}
                     {/* Read a Bad Situation is quick investigation in a dangerous situation */}
                     <td><input type="number" name="sharpMod" id="sharpMod" max="3"/></td>
                 </tr>
                 <tr>
-                    <td>Tough</td>
+                    <td>Tough<br />
+                <ul>
+                    <li>Kick Some Ass</li>
+                    <li>Protect Someone</li>
+                </ul></td>
                     {/* insert modal for Kick Some Ass and Protect Someone */}
                     {/* Kick Some Ass is a roll for fighting, resulting in both opponents trading their damage */}
                     {/* Protect Someone is used for taking damage that was intended for another */}
