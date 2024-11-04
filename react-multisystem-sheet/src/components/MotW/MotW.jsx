78938ce61c0674e193d9a86aeb386d17df208863
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-11-03 21:57:06 -0500

    adding row in motw sheet for rolling with listed modifier per stat

--- react-multisystem-sheet/src/components/MotW/MotW.jsx
+++ react-multisystem-sheet/src/components/MotW/MotW.jsx
@@ -16,6 +16,7 @@ export const MotW = () => {
                 <tr>
                     <th>Ability</th>
                     <th>Modifier</th>
+                    <th>Roll Button</th>
                 </tr>
                 <tr>
                  <td>Charm<br />
@@ -26,6 +27,7 @@ export const MotW = () => {
                  {/* insert modal for Manipulate Someone */}
                  {/* Manipulate Someone requires you to give what you want them to do and why they should */}
                  <td><input type="number" id="charmMod" max="3"/></td>   
+                 <td><button>Roll Charm</button></td>
                 </tr>
                 <tr>
                     <td>Cool<br />
@@ -37,6 +39,7 @@ export const MotW = () => {
                     {/* Act Under Pressure is a reactionary roll, typically requested by Keeper */}
                     {/* Help Out is used to increase another player's roll by +1 */}
                     <td><input type="number" name="coolMod" id="coolMod" max="3"/></td>
+                    <td><button>Roll Cool</button></td>
                 </tr>
                 <tr>
                     <td>Sharp<br />
@@ -48,6 +51,7 @@ export const MotW = () => {
                     {/* Investigate a Mystery is used to investigate when not in danger */}
                     {/* Read a Bad Situation is quick investigation in a dangerous situation */}
                     <td><input type="number" name="sharpMod" id="sharpMod" max="3"/></td>
+                    <td><button>Roll Sharp</button></td>
                 </tr>
                 <tr>
                     <td>Tough<br />
@@ -59,11 +63,13 @@ export const MotW = () => {
                     {/* Kick Some Ass is a roll for fighting, resulting in both opponents trading their damage */}
                     {/* Protect Someone is used for taking damage that was intended for another */}
                     <td><input type="number" name="toughMod" id="toughMod" max="3" /></td>
+                    <td><button>Roll Tough</button></td>
                 </tr>
                 <tr>
                     <td>Weird</td>
                     {/* insert modal for Weird moves */}
                     <td><input type="number" name="weirdMod" id="weirdMod" max="3" /></td>
+                    <td><button>Roll Weird</button></td>
                 </tr>
             </table>
             </form>
