0a7d4cb95fc5e11112ebd2cf80019e27ef5f3826
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-12-07 15:10:02 -0500

    resolved color preview issue with updated naming convention removing hyphen in className

--- react-multisystem-sheet/src/components/Navbar/Navbar.jsx
+++ react-multisystem-sheet/src/components/Navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import styles from './Navbar.module.css'
 
 export const Navbar = () => {
     return (
-        <div class="tabs">
-            <nav class="tab-nav">
-            <ul class="tab-list" id="tab-list">
+        <div className={styles.tabs}>
+            <nav className={styles.tabNav}>
+                <ul className={styles.tabList} id="tab-list">
                 <li><button>DnD5e</button></li>
                 <li><button>MotW</button></li>
                 <li><button>Persona</button></li>
