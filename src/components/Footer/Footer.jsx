a147a41cdecc87b51a006bd9c0bfa3a43fd4f8a2
Dylan Thrasher <thrasher_dylan@yahoo.com>
2025-01-05 22:24:22 -0500

    added request form input section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,11 @@ export const Footer = () => {
             <div>
                 <h2>Want to add a system?</h2>
                 <p>Feel free to send a request below!</p>
+                <form action="submit">
+                    <label htmlFor="request"> System Requested
+                        <input type="text" name="request" id="request" />
+                    </label>
+                </form>
             </div>
         </footer>
     )
