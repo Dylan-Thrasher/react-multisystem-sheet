c85b6106f8ace7e0e57cdc32d70cfd4310c23ab5
Dylan Thrasher <thrasher_dylan@yahoo.com>
2025-01-15 21:42:40 -0500

    added rules for Roll for Shoes along with link to site

--- src/components/RollForShoes/RollForShoes.jsx
+++ src/components/RollForShoes/RollForShoes.jsx
@@ -4,7 +4,15 @@ import styles from './RollForShoes.module.css'
 export const RollForShoes = () => {
     return (
         <section>
-            
+            <a href="https://rollforshoes.com/">Roll for Shoes official page</a>
+            <ol>
+            <li>Say what you do and roll a number of D6s, determined by the level of relevant skill you have.</li>
+            <li>If the sum of your roll is higher than an opposing roll, the thing you wanted to happen, happens.</li>
+            <li>At start, you have only one skill: Do Anything 1.</li>
+            <li>If you roll all 6s, you get a new skill specific to the action, one level higher than the one you used.</li>
+            <li>For every roll you fail, you get 1 XP.</li>
+            <li>XP can be used to change a die into a 6 for advancement purposes only.</li>
+            </ol>
         </section>
     )
 }
