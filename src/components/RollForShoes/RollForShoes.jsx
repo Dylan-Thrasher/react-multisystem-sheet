e364ae2ab39107afab8eba3c66bce2c80f529072
Dylan Thrasher <thrasher_dylan@yahoo.com>
2025-01-27 22:47:39 -0500

    adding add row button to rollforshoes

--- src/components/RollForShoes/RollForShoes.jsx
+++ src/components/RollForShoes/RollForShoes.jsx
@@ -56,6 +56,9 @@ import {RollTable} from "./RollTable";
                                 </td>
                             </tr>
                         ))}
+                        <tr>
+                            <button onClick={() => addRow()}>Add Row</button>
+                        </tr>
                     </tbody>
                 </table>
                 {/* Create skill and level */}
